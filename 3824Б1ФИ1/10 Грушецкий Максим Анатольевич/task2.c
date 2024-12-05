34577053ff4028eb9ec902eaee9adffcb5b5906a
Grushet187 <maxgrush2006@gmail.com>
2024-12-05 10:12:33 +0300

    Update task2.c

--- 3824Б1ФИ1/10 Грушецкий Максим Анатольевич/task2.c
+++ 3824Б1ФИ1/10 Грушецкий Максим Анатольевич/task2.c
@@ -1,6 +1,15 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#define UINT 4294967295
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+	long int c = a * b;
+	if (b > UINT/a)
+	{
+		return UINT;
+	}
+	if (b < UINT/a)
+	{
+		return c;
+	}
 }
