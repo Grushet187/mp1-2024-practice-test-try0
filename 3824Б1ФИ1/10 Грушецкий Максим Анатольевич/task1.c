fc225fa1d14ab4d4e4c201dad2f44a784bf3cac8
Grushet187 <maxgrush2006@gmail.com>
2024-12-05 10:10:27 +0300

    Update task1.c

--- 3824Б1ФИ1/10 Грушецкий Максим Анатольевич/task1.c
+++ 3824Б1ФИ1/10 Грушецкий Максим Анатольевич/task1.c
@@ -1,7 +1,20 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+	if (a >= INT_MAX-b)
+	{
+		return INT_MAX;
+	}
+	if (a <= INT_MIN-b)
+	{
+		return INT_MIN;
+	}
+	if (a<INT_MAX-b && a>INT_MIN-b)
+	{
+		return a+b;
+	}
 }
